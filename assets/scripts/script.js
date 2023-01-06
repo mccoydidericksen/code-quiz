4fee947275eaf72b425c9d3f0b94819adee2e75d
McCoy Didericksen <mccoy.did@gmail.com>
2023-01-05 17:36:41 -0700

    add timer and check answer logic

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,36 +5,88 @@ var optionA = document.getElementById("optionA");
 var optionB = document.getElementById("optionB");
 var optionC = document.getElementById("optionC");
 var optionD = document.getElementById("optionD");
+var seconds = document.getElementById("seconds");
+var timerEl = document.getElementById("timer");
 var questionIndex = 0;
+var correctCount = 0;
+var secondsLeft = 20;
 
-function addIdtoQuestions(){
-    for(var i=0; i<questions.length; i++){
-        questions[i].id = i;
+function clearAnswer() {
+    var options = document.getElementsByName("option");
+    for(var i=0; i<options.length; i++){
+        options[i].checked = false;
+        document.getElementById("option" + options[i].id.toUpperCase()).style.backgroundColor = "white";
     }
 }
-
-addIdtoQuestions();
 
 function checkAnswer(event) {
     event.preventDefault();
-    var options = document.getElementsByName("option");
-    for(var i=0; i<options.length; i++){
-        if(options[i].checked){
-            var selectedAnswer = options[i]
-        }
+    if (submitButton.textContent === "Start Quiz") {
+        toggleTimer(1);
+        setTimeout(clearAnswer, 1000);
+        setTimeout(nextQuestion, 1000);
+        submitButton.textContent = "Submit Answer";
     }
-    if(selectedAnswer){
-        console.log(selectedAnswer)
+    else {
+        var options = document.getElementsByName("option");
+        for(var i=0; i<options.length; i++){
+            if(options[i].checked){
+                var selectedAnswer = options[i]
+            }
+        }
+        if(selectedAnswer){
+            console.log(selectedAnswer.id);
+            console.log(questions[questionIndex-1]);
+            if(selectedAnswer.id === questions[questionIndex -1].correctAnswer){
+                document.getElementById("option" + selectedAnswer.id.toUpperCase()).style.backgroundColor = "green";
+                correctCount++;
+            }
+            else{
+                document.getElementById("option" + selectedAnswer.id.toUpperCase()).style.backgroundColor = "red";
+            }
+        }
+        setTimeout(clearAnswer, 1000);
+        setTimeout(nextQuestion, 1000);
     }
 }
 
-function nextQuestion() {
-    questionEl.textContent = questionIndex + 1 + ". " + questions[questionIndex].question;
-    optionA.textContent = questions[questionIndex].answers.a;
-    optionB.textContent = questions[questionIndex].answers.b;
-    optionC.textContent = questions[questionIndex].answers.c;
-    optionD.textContent = questions[questionIndex].answers.d;   
+function endQuiz() {
+    questionEl.textContent = "Quiz Complete!";
+    optionA.textContent = "Correct Answers: " + correctCount;
+    optionB.textContent = "Incorrect Answers: " + (questions.length - correctCount);
+    optionC.textContent = "Score: " + correctCount;
+    optionD.style.display = "none";
+    submitButton.style.display = "none";
 }
-nextQuestion();
+
+function nextQuestion() {
+        questionEl.textContent = questionIndex + 1 + ". " + questions[questionIndex].question;
+        optionA.textContent = questions[questionIndex].answers.a;
+        optionB.textContent = questions[questionIndex].answers.b;
+        optionC.textContent = questions[questionIndex].answers.c;
+        optionD.textContent = questions[questionIndex].answers.d;
+        questionIndex++;
+}
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timerEl.textContent = "Time Remaining: " + secondsLeft;
+      if(secondsLeft <= 0) {
+        clearInterval(timerInterval);
+        endQuiz();
+      } else if(questionIndex === questions.length){
+        secondsLeft = 0;
+        clearInterval(timerInterval);
+        endQuiz();
+        }
+    }, 1000);
+  }
+
+function toggleTimer(toggle) {
+    if(toggle){
+        setTime();
+    }
+}
 
 submitButton.addEventListener("click", checkAnswer);
